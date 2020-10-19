f12e68d76f668700dbba3f6ab275309c44f37c8b
ankutalev <a.kutalev@g.nsu.ru>
2020-10-20 01:28:14 +0700

    Fix memoization

--- src/nsufit/task2.clj
+++ src/nsufit/task2.clj
@@ -1,42 +1,59 @@
 (ns nsufit.task2)
 
+(defn trapezoid-square
+  [x1 x2 h]
+  "Calculate trapzeoid square with sizes x1, x2 and midsegment h"
+  (* (/(+ x1 x2) 2) h)
+  )
+
 (defn trapezoids-sum
-  "Calculates integral(f) from 0 to xcur with step h."
-  [f, h, xcur]
-  (let [
-        square (fn [x0, x1] (*(/(+ (f x0) (f x1)) 2) h)),
-        curried (partial trapezoids-sum f h),
-        next (- xcur h)
-        ]
-    (if (<= xcur h)
-        (* (/ (square xcur 0) h) xcur)
-      (+ (square xcur next) (curried next))
+  [f h xcur]
+  "Calculate integral of f with trapezoid method with step h from 0 to xcur"
+  (let [next (- xcur h)]
+     (if (< xcur h)
+       0
+       (+ (trapezoid-square (f xcur) (f next) h) (trapezoids-sum f h next))
+       )
+   ))
+
+
+(def trapezoids-sum-mem
+  "Memoized version of trapezoids-sum"
+   (memoize
+     (fn [f h xcur]
+      (let [next (- xcur h)]
+        ;debug print to see memoization
+        (println "trapezoids-sum(" xcur ")")
+         (if (< xcur h)
+           0
+          (+ (trapezoid-square (f xcur) (f next) h) (trapezoids-sum-mem f h next))
+          ))
       )
+     )
+  )
+
+
+(defn generic-integral
+  [f, integrate-func]
+  (let [h 0.5]
+    (fn [x]
+      (let [nearest (* h (Math/floor (/ x h))), restx (- x nearest)]
+        (+ (integrate-func f h nearest) (trapezoid-square (f nearest) (f x) restx))
+        ))
     )
   )
-
-(def mem-trapezoids-sum
-  "Memoized version of trapezoids-sum"
-  (memoize trapezoids-sum)
-  )
-
 
 (defn get-integral
   [f]
   "Integrate operator - takes f and returns function which
   will calculate integral from 0 to it argument"
-  (let [h 0.01]
-    (fn [x]
-      (trapezoids-sum f h x))
-    )
+  (generic-integral f trapezoids-sum)
   )
 
 (defn get-integral-mem
-  "Memoized version of get-integral"
   [f]
-  (let [h 0.01]
-    (fn [x] (mem-trapezoids-sum f h x))
-    )
+  "memoized version of get-integral"
+  (generic-integral f trapezoids-sum-mem)
   )
 
 (defn calculus
@@ -45,63 +62,3 @@
   (let [integral (integral-getter f)]
     (doall (map integral (range 1 n))))
   )
-
-
-; Benchmarks for memoize (h=1)
-;(def a (time (calculus identity get-integral 3000)))
-;"Elapsed time: 4831.1479 msecs"
-;(def a (time (calculus (fn [x] (* x x)) get-integral 3000)))
-;"Elapsed time: 4317.1479 msecs"
-;(def a (time (calculus (fn [x] (* x x)) get-integral 3000)))
-;"Elapsed time: 4320.7465 msecs"
-
-;(def a (time (calculus (fn [x] (* x x)) get-integral-mem 3000)))
-;"Elapsed time: 3306.1826 msecs"
-;(def a (time (calculus (fn [x] (* x x)) get-integral-mem 3000)))
-;"Elapsed time: 4.3511 msecs"
-;(def a (time (calculus (fn [x] (* x x)) get-integral-mem 3000)))
-;"Elapsed time: 4.5769 msecs"
-;(def a (time (calculus (fn [x] (* x x)) get-integral-mem 4000)))
-;"Elapsed time: 3514.1047 msecs"
-;(def a (time (calculus (fn [x] (* x x)) get-integral-mem 4000)))
-;"Elapsed time: 3.84 msecs"
-
-;(def a (time (calculus identity get-integral 4000)))
-;"Elapsed time: 7692.9478 msecs"
-;(def a (time (calculus identity get-integral 4000)))
-;"Elapsed time: 7828.0422 msecs"
-
-
-(defn partials
-  ([f, h, x] (partials f h x x 0))
-  ([f, h, x, xcur, prevsquares]
-   (let [fcur (f xcur),
-         hnext (- xcur h),
-         squares (+ fcur prevsquares (f hnext))
-         ]
-     (if (> xcur h)
-       (lazy-seq (cons squares (partials f h x  hnext squares)))
-     (list (* (/ (+ prevsquares fcur) 2) h))
-       )
-     )
-   )
-  )
-
-(defn get-integral-partial
-  [f]
-  (fn [x]
-    (let [h 0.01]
-      (first (take-last 1 (partials f h x)))
-      )
-    )
-  )
-
-; Benchmarks for get-integral-partial (h=1)
-;(time (calculus identity get-integral-partial 3000))
-;"Elapsed time: 887.8372 msecs"
-;(time (calculus identity get-integral-partial 3000))
-;"Elapsed time: 818.718 msecs"
-;(time (calculus identity get-integral-partial 4000))
-;"Elapsed time: 1382.0636 msecs"
-;(time (calculus identity get-integral-partial 4000))
-;"Elapsed time: 1359.1425 msecs"
