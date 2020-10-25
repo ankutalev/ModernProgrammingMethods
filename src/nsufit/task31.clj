b9c89f29b0b2bfd10d3f4f7ab45277d6dc20e3c7
ankutalev <a.kutalev@g.nsu.ru>
2020-10-26 01:48:34 +0700

    fix parallelism at 3.1

--- src/nsufit/task31.clj
+++ src/nsufit/task31.clj
@@ -21,9 +21,15 @@
       ))
   )
 
-(defn my-parallel-filter
-  [block-size, f, coll]
-    (mapcat deref (doall (map (fn [block] (future (my-filter f block))) (my-partition block-size coll))
-  ))
+(defn my-pred
+  [x]
+  (println "Sleeping 5 sec At thread" (.getName (Thread/currentThread)))
+  (Thread/sleep 5000)
+  (identity x)
   )
 
+(defn my-parallel-filter
+  [block-size, f, coll]
+  (let [blocks (my-partition block-size coll)]
+    (mapcat deref (doall (map (fn [block] (future (doall (my-filter f block)))) blocks))))
+  )
